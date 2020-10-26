76f35bf3877d1af07812e1738fbd7d27e6f0ad8d
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-10-26 16:21:27 +0100

    created repository

---
+++ src/test/java/com/riot/matchtracker/MatchtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riot.matchtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchtrackerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
